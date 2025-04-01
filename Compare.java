99d4f6cb7ca7c385acacd05b47b91aef6ef91f74
Sneha Modi <snehamodi40@gmail.com>
2025-04-01 19:27:50 +0530

    Add files via upload

---
+++ Compare.java
@@ -0,0 +1,15 @@
+ class Compare{
+
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+
+        if (num1 > num2) {
+            System.out.println("num1 is greater than num2");
+        } else if (num1 < num2) {
+            System.out.println("num1 is less than num2");
+        } else {
+            System.out.println("num1 is equal to num2");
+        }
+    }
+}
